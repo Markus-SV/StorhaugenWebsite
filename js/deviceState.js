4f9eae3e86d6fd226e059b251be6a570df9fb4e1
Markus-SV <72076801+Markus-SV@users.noreply.github.com>
2025-12-04 22:31:46 +0000

    Deploying to gh-pages from @ Markus-SV/StorhaugenWebsite@4f0ab3901f911518ddf071fe8cf55f7ca43070bd 🚀

---
+++ js/deviceState.js
@@ -0,0 +1,17 @@
+﻿window.deviceState = {
+    get: function (key) {
+        return localStorage.getItem(key);
+    },
+    set: function (key, value) {
+        localStorage.setItem(key, value);
+    },
+    remove: function (key) {
+        localStorage.removeItem(key);
+    },
+    getPreferredTheme: function () {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            return 'dark';
+        }
+        return 'light';
+    }
+};
